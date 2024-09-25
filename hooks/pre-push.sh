40de34290b229c1ad6c1745e2b68c8bd84b5956a
www778878net <657225485@qq.com>
2024-09-25 15:52:30 +0800

    fix

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -27,25 +27,7 @@ cd "$work_dir"
 # 激活虚拟环境
 source .venv/Scripts/activate
 
-if [ "$current_branch" = "develop" ]; then
-    echo "当前分支是 develop。运行 pre-push 检查... (执行 ID: $EXECUTION_ID)" 
-
-    echo "切换到 main 分支并更新... (执行 ID: $EXECUTION_ID)"
-    git checkout main
-    #git pull origin main
-
-    echo "获取 develop 分支最后一次提交信息... (执行 ID: $EXECUTION_ID)"
-    last_commit_msg=$(git log develop -1 --pretty=%B)
-
-    echo "合并 develop 分支，使用 --squash 选项... (执行 ID: $EXECUTION_ID)"
-    git merge --squash develop
-
-    echo "提交更改... (执行 ID: $EXECUTION_ID)"
-    git commit -m "$last_commit_msg"
-
-    echo "合并到 main 成功完成 (执行 ID: $EXECUTION_ID)"
-
-elif [ "$current_branch" = "main" ]; then
+if [ "$current_branch" = "main" ]; then
     echo "当前分支是 main。运行... (执行 ID: $EXECUTION_ID)"
     echo "运行测试... (执行 ID: $EXECUTION_ID)"
     pytest
@@ -53,17 +35,7 @@ elif [ "$current_branch" = "main" ]; then
         echo "测试失败，推送已中止 (执行 ID: $EXECUTION_ID)"
         exit 1
     fi
-    echo "测试通过成功 (执行 ID: $EXECUTION_ID)"
-
-    echo "切换到 develop 分支并合并 main... (执行 ID: $EXECUTION_ID)"
-    git checkout develop 
-    git merge main
-    if [ $? -ne 0 ]; then
-        echo "切换到 develop 分支并合并 main 失败，推送已中止 (执行 ID: $EXECUTION_ID)"
-        exit 1
-    fi
-    echo "切换到 develop 分支并合并 main 成功完成 (执行 ID: $EXECUTION_ID)"
-
+    echo "测试通过成功 (执行 ID: $EXECUTION_ID)"  
  
 
 else
